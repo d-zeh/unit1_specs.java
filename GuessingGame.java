b1f37e6474f317c7f7f9ca91b30b534d1e499269
Daniel Zeh <dfzeh1@gmail.com>
2023-08-16 20:01:15 -0500

    first commit

---
+++ GuessingGame.java
@@ -0,0 +1,9 @@
+public class GuessingGame {
+    public static void main(String[] args) {
+
+        System.out.println("Greetings, player.... or are you a playa'?");
+        System.out.println("Your name is?");
+
+
+    }
+}
